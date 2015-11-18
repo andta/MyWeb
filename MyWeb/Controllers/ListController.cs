de1a2e1d5e8a86e7d69e4f6a1b909bce163256ab
andta <admin@andta.com>
2015-11-18 17:34:49 +0800

    取

--- MyWeb/Controllers/ListController.cs
+++ MyWeb/Controllers/ListController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using MyWeb.Models;
 
+
 namespace MyWeb.Controllers
 {
     public class ListController : Controller
@@ -17,6 +18,19 @@ namespace MyWeb.Controllers
 
             return View(db.DbSet_WenZhang.ToList());
         }
+        public ActionResult ListPanel(int skipCount, int takeCount)
+        {
+            //var m = from us in db.DbSet_WenZhang where us.BiaoTi == "中国" && us.JianYao == "中国" select us;
+            var m = from us in db.DbSet_WenZhang  select us;
+            //if (m.Count() != 0)
+            //{
+
+            //}
+          
+            var list = m.ToList().Skip(skipCount).Take(takeCount).ToList();
+            return PartialView(list);
+        }
+ 
 
         // GET: Default/Details/5
         //详细内容
