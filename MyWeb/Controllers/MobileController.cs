44d1e20269ce8d44c01d8923c3d0de9f1bdf0ed2
andta <admin@andta.com>
2015-11-21 16:41:40 +0800

    修改了移动端

--- MyWeb/Controllers/MobileController.cs
+++ MyWeb/Controllers/MobileController.cs
@@ -8,33 +8,33 @@ using MyWeb.Models;
 
 namespace MyWeb.Controllers
 {
-    public class ListController : Controller
+    public class MobileController : Controller
     {
         private ModelContext db = new ModelContext();
         // GET: Default
-        //列表
+        //列表全部数据
         public ActionResult Index()
         {
-
             return View(db.DbSet_WenZhang.ToList());
         }
+        // GET: Mobile/ListPanel/n,m
+        //列表加载更多，从第N条到第M条数据
         public ActionResult ListPanel(int skipCount, int takeCount)
         {
             //var m = from us in db.DbSet_WenZhang where us.BiaoTi == "中国" && us.JianYao == "中国" select us;
-            var m = from us in db.DbSet_WenZhang  select us;
-            //if (m.Count() != 0)
-            //{
-
-            //}
-          
+            var m = from us in db.DbSet_WenZhang select us;
             var list = m.ToList().Skip(skipCount).Take(takeCount).ToList();
             return PartialView(list);
         }
- 
-
         // GET: Default/Details/5
         //详细内容
         public ActionResult Details(int id)
+        {
+            return View();
+        }
+        // GET: Default/Details/5
+        //关于信息页面
+        public ActionResult AboutInfo()
         {
             return View();
         }
